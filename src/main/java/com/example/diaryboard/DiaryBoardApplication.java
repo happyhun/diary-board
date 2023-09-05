115da15b91ae306784adca84178bf6a4dd573ded
happyhun <younghun.ai@gmail.com>
2023-09-05 11:25:19 +0900

    add: spring initialize

---
+++ src/main/java/com/example/diaryboard/DiaryBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.diaryboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diaryboard/DiaryBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diaryboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
