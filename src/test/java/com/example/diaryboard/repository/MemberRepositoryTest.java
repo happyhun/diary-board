522451eb4c180496796e95b3c9f1404b27fa9bde
happyhun <younghun.ai@gmail.com>
2023-09-08 18:33:34 +0900

    feat: create MemberRepository

---
+++ src/test/java/com/example/diaryboard/repository/MemberRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.diaryboard.repository;
+
+import com.example.diaryboard.entity.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    void 회원_생성_조회() {
+        // given
+        String name = "임시완";
+        String email = "test@gmail.com";
+        String password = "test123!@#";
+
+        Member member = Member.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+
+        // when
+        Member saveMember = memberRepository.save(member);
+        Optional<Member> findMember = memberRepository.findById(saveMember.getId());
+
+        // then
+        assertThat(findMember).isNotEmpty();
+        assertThat(findMember.get().getName()).isEqualTo(member.getName());
+        assertThat(findMember.get().getEmail()).isEqualTo(member.getEmail());
+        assertThat(findMember.get().getPassword()).isEqualTo(member.getPassword());
+    }
+
+    @Test
+    void 회원_생성_삭제() {
+        // given
+        String name = "임시완";
+        String email = "test@gmail.com";
+        String password = "test123!@#";
+
+        Member member = Member.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+
+        // when
+        Member saveMember = memberRepository.save(member);
+        memberRepository.delete(saveMember);
+        Optional<Member> findMember = memberRepository.findById(saveMember.getId());
+
+        // then
+        assertThat(findMember).isEmpty();
+    }
+}
