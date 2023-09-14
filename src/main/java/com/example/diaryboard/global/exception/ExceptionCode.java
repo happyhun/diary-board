c5d1bd2b98581ca3f7ea004d5f592231215f73f9
happyhun <younghun.ai@gmail.com>
2023-09-14 22:18:09 +0900

    feat: add token exception code

--- src/main/java/com/example/diaryboard/global/exception/ExceptionCode.java
+++ src/main/java/com/example/diaryboard/global/exception/ExceptionCode.java
@@ -12,7 +12,9 @@ public enum ExceptionCode {
     INVALID_JSON_FORMAT(BAD_REQUEST, "JSON_001", "잘못된 JSON 요청입니다"),
     DUPLICATED_EMAIL(CONFLICT, "MEMBER_001", "중복된 이메일입니다"),
     DUPLICATED_NICKNAME(CONFLICT, "MEMBER_002", "중복된 닉네임입니다"),
-    UNAUTHORIZED_LOGIN(UNAUTHORIZED, "MEMBER_003", "잘못된 로그인 요청입니다");
+    UNAUTHORIZED_LOGIN(UNAUTHORIZED, "MEMBER_003", "잘못된 로그인 요청입니다"),
+    INVALID_TOKEN(UNAUTHORIZED, "TOKEN_001", "유효하지 않은 토큰입니다"),
+    EXPIRED_TOKEN(UNAUTHORIZED, "TOKEN_002", "만료된 토큰입니다");
 
     private final HttpStatus status;
     private final String code;
